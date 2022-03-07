26dee97d7bf108ca4d1b42064b91f4b52da2c387
= <=>
2022-03-07 15:37:03 +0800

    events to event

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -5,27 +5,32 @@ import Kalend, { CalendarView } from "kalend";
 import "kalend/dist/styles/index.css"; // import styles
 
 const Calendar = ({ data }) => {
-  console.log(data);
+  const { edges: events } = data.allMarkdownRemark;
+  const final_events = [];
+  events.forEach((element) => {
+    final_events.push(element.node.frontmatter);
+  });
+  console.log(final_events);
   const onEventClick = (e) => {
     console.log(e);
   };
-  const events = [
-    {
-      id: 1,
-      startAt: "2022-02-27T18:00:00.000Z",
-      endAt: "2021-02-27T19:00:00.000Z",
-      timezoneStartAt: "Europe/Berlin", // optional
-      summary: "Тасартлаа ууна",
-      color: "blue",
-      calendarID: "work",
-      url: "saa",
-    },
-  ];
+  // const events = [
+  //   {
+  //     id: 1,
+  //     startAt: "2022-02-27T18:00:00.000Z",
+  //     endAt: "2021-02-27T19:00:00.000Z",
+  //     timezoneStartAt: "Europe/Berlin", // optional
+  //     summary: "Тасартлаа ууна",
+  //     color: "blue",
+  //     calendarID: "work",
+  //     url: "saa",
+  //   },
+  // ];
   return (
     <Kalend
       onEventClick={onEventClick}
       // onNewEventClick={onNewEventClick}
-      events={events}
+      events={final_events}
       initialDate={new Date().toISOString()}
       hourHeight={60}
       initialView={CalendarView.MONTH}
